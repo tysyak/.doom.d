00dba5735f9f072c4080c1b9a36ae7b3879282cb
Cristian Romero Andrade <mascrit@gmail.com>
2021-08-07 18:57:33 -0500

    fira code
    Signed-off-by: Cristian Romero Andrade <mascrit@gmail.com>

--- config.el
+++ config.el
@@ -19,9 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka" :size 12 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 13))
-
+(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
