b31cf791af8a5b9c326c8b948b226f0971fac26d
Cristian Romero Andrade <mascrit@gmail.com>
2021-08-11 20:41:31 -0500

    accommodate variables

--- custom.el
+++ custom.el
@@ -3,20 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LANGUAGE-NAME "spanish")
+
  '(LaTeX-command-style
    '(("" "%(PDF)%(latex) -shell-escape %(file-line-error) %(extraopts) %(output-dir) %S%(PDFout)")))
  '(TeX-view-program-list '(("xreader" "xreader --page-index=%(outpage) %o")))
  '(TeX-view-program-selection '((output-pdf "xreader")))
+
  '(org-latex-listings 'minted)
- '(confirm-kill-emacs nil)
  '(org-latex-pdf-process
    '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f" "bibtex %b" "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f" "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
- '(TeX-view-program-list '(("xreader" "xreader --page-index=%(outpage) %o")))
- '(TeX-view-program-selection '((output-pdf "xreader")))
- '(package-selected-packages '(vcard org-vcard vala-mode nasm-mode ag))
+ '(org-roam-directory "~/org/roam")
+
+ '(confirm-kill-emacs nil)
+
  '(wakatime-cli-path "/usr/bin/wakatime")
  '(wakatime-python-bin nil)
- '(LANGUAGE-NAME "spanish")
+
  )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
