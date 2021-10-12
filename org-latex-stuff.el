2e933daebed71054e3e813d8ce432e16499780eb
Cristian Romero Andrade <mascrit@gmail.com>
2021-10-11 20:25:06 -0500

    ieeetran, koma-script para latex

--- org-latex-stuff.el
+++ org-latex-stuff.el
@@ -4,12 +4,24 @@
   (add-to-list 'org-latex-classes
                '("IEEEtran"
                  "\\documentclass{IEEEtran}"
+                 ("\\section{%s}" . "\\section{%s}")
+                 ("\\subsection{%s}" . "\\subsection{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph{%s}")))
+  )
+
+(with-eval-after-load 'ox-latex
+  (add-to-list 'org-latex-classes
+               '("scrartcl"
+                 "\\documentclass{scrartcl}"
                  ("\\section{%s}" . "\\section*{%s}")
                  ("\\subsection{%s}" . "\\subsection*{%s}")
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
   )
+
 (with-eval-after-load 'ox-latex
   (setq org-export-latex-listings 'minted)
   (add-to-list 'org-export-latex-packages-alist '("" "minted"))
