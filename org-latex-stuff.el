98c6977e6d110f98917c82b7f5ec7b4c1152b517
Cristian Romero Andrade <mascrit@gmail.com>
2021-10-26 18:50:21 -0500

    Configuración de latex al exportar

--- org-latex-stuff.el
+++ org-latex-stuff.el
@@ -1,9 +1,36 @@
 ;;; org-latex-stuff.el -*- lexical-binding: t; -*-
 
+
 (with-eval-after-load 'ox-latex
   (add-to-list 'org-latex-classes
                '("IEEEtran"
-                 "\\documentclass{IEEEtran}"
+                 "\\documentclass{IEEEtran}
+\\usepackage[left=1.5cm,right=1.5cm,letterpaper]{geometry}
+\\usepackage[spanish,es-nodecimaldot,es-tabla]{babel}
+\\usepackage[utf8]{inputenc}
+\\usepackage{blindtext}
+\\usepackage{subfigure}
+\\usepackage[most]{tcolorbox}
+\\usepackage{etoolbox}
+\\usepackage{minted}
+\\usepackage{hyperref}
+\\usepackage{xcolor}
+\\definecolor{LightGray}{gray}{0.9}
+\\definecolor{DarkGray}{HTML}{191919}
+\\definecolor{custom}{HTML}{F8F8F8}
+\\usepackage{caption}
+\\usepackage{breqn}
+\\usemintedstyle{emacs}
+\\usepackage[ruled,vlined]{algorithm2e}
+\\newenvironment{code}{\\captionsetup{type=listing}}{}
+\\setminted{frame=lines,breaklines=true,bgcolor=custom,fontsize=\\scriptsize,autogobble}
+\\renewcommand{\\listingscaption}{Código}
+\\renewcommand\\listoflistingscaption{Índice de \\listingscaption\\@s}
+\\BeforeBeginEnvironment{minted}{\\begin{code}}
+\\AfterEndEnvironment{minted}{\\end{code}}
+[NO-DEFAULT-PACKAGES]
+[EXTRA]
+"
                  ("\\section{%s}" . "\\section{%s}")
                  ("\\subsection{%s}" . "\\subsection{%s}")
                  ("\\subsubsection{%s}" . "\\subsubsection{%s}")
@@ -14,12 +41,41 @@
 (with-eval-after-load 'ox-latex
   (add-to-list 'org-latex-classes
                '("scrartcl"
-                 "\\documentclass{scrartcl}"
+                 "\\documentclass{scrartcl}
+\\usepackage[left=1.5cm,right=1.5cm,letterpaper]{geometry}
+\\usepackage[spanish,es-nodecimaldot,es-tabla]{babel}
+\\usepackage[utf8]{inputenc}
+\\usepackage{blindtext}
+\\usepackage{multicol}
+\\usepackage{subfigure}
+\\usepackage[most]{tcolorbox}
+\\usepackage{etoolbox}
+\\usepackage{minted}
+\\usepackage{hyperref}
+\\usepackage{xcolor}
+\\definecolor{LightGray}{gray}{0.9}
+\\definecolor{DarkGray}{HTML}{191919}
+\\definecolor{custom}{HTML}{F8F8F8}
+\\usepackage{caption}
+\\usepackage{breqn}
+\\usemintedstyle{emacs}
+\\usepackage[ruled,vlined]{algorithm2e}
+\\newenvironment{code}{\\captionsetup{type=listing}}{}
+\\setminted{frame=lines,breaklines=true,bgcolor=custom,fontsize=\\scriptsize}
+\\renewcommand{\\listingscaption}{Código}
+\\renewcommand\\listoflistingscaption{Índice de \\listingscaption\\@s}
+\\BeforeBeginEnvironment{listing}{\\begin{code}}
+\\AfterEndEnvironment{listing}{\\end{code}}
+\\BeforeBeginEnvironment{minted}{\\begin{code}}
+\\AfterEndEnvironment{minted}{\\end{code}}
+[NO-DEFAULT-PACKAGES]
+[EXTRA]"
                  ("\\section{%s}" . "\\section*{%s}")
                  ("\\subsection{%s}" . "\\subsection*{%s}")
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+               )
   )
 
 (with-eval-after-load 'ox-latex
